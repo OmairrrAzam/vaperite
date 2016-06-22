d4bcb8b6391c6fbe702de619b391b4a51c2cb1d2
Omair <omair@gems.techverx.com>
2016-06-22 23:32:39 +0500

    Initial Commit

---
+++ vaperite/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  vaperite
+//
+//  Created by Apple on 22/06/2016.
+//  Copyright © 2016 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ vaperite/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  vaperite
+//
+//  Created by Apple on 22/06/2016.
+//  Copyright © 2016 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
